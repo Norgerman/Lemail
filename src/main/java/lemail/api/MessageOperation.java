258ab99aa77082895f9e358e3c7b368956979622
lilelr <lilelr@163.com>
2015-07-08 15:07:34 +0800

    获取通知信息api完成，返回按时间排序的数组

--- src/main/java/lemail/api/MessageOperation.java
+++ src/main/java/lemail/api/MessageOperation.java
@@ -5,9 +5,11 @@ import lemail.model.User;
 import lemail.utils.Action;
 import lemail.utils.DBSession;
 import org.hibernate.Session;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -62,10 +64,40 @@ public class MessageOperation {
         return null;
     }
 
+    public String getmsgs(){
+//        Session s = DBSession.getSession();
+        try{
+            check();
+            Integer uid = (Integer) Action.getSession("uid");
+            List<Message> msgList = DBSession.find_list(Message.class, Order.desc("date"),Restrictions.eq("to", uid));
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            for (Message msg : msgList) {
+                sb.append(msg.toJson());
+                sb.append(',');
+            }
+            if (sb.length() > 1) {
+                sb.setCharAt(sb.length() - 1, ']');
+            } else {
+                sb.append("]");
+            }
+            Action.echojson(0, "success", sb.toString());
+        }catch (ApiException ex){
+            return Action.error(ex.getId(), ex.getMessage());
+        }catch (Exception ex){
+            ex.printStackTrace();
+            Action.error(-1, "未知异常");
+        }
+//        finally {
+//            s.close();
+//        }
+        return null;
+    }
+
     private void check() throws ApiException {
         Integer uid = (Integer) Action.getSession("uid");
         if (uid == null)
-            throw new ApiException(401, "发送者未登录");
+            throw new ApiException(401, "用户未登录");
 
     }
 }
