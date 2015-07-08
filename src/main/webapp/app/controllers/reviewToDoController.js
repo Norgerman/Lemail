c339c4b0ebab4c967f41b0c3d2caecdd914f28c1
vvliebe@gmail.com <vvliebe@gmail.com>
2015-07-08 14:10:48 +0800

    add reviewer html

--- src/main/webapp/app/controllers/reviewToDoController.js
+++ src/main/webapp/app/controllers/reviewToDoController.js
@@ -4,5 +4,68 @@
 LeMailModule.controller('reviewToDoController', ['$scope','$http','$location',function($scope, $http, $location){
     $scope.onPageLoad = function() {
 
-    }
+    };
+
+    $scope.review_list = [
+        {
+            "id": 2,
+            "subject": "xx",
+            "state": 3,
+            "date": "2015-07-06 16:23:23",
+            "to": "594254655@qq.com",
+            "tag": null,
+            "checker": {
+                "id": 15,
+                "name": "a"
+            },
+            "sender": {
+                "id": 15,
+                "name": "a"
+            },
+            "reply": {
+                "id": 14,
+                "subject": "xxxx"
+            }
+        },
+        {
+            "id": 2,
+            "subject": "xx",
+            "state": 4,
+            "date": "2015-07-06 16:23:23",
+            "to": "594254655@qq.com",
+            "tag": null,
+            "checker": {
+                "id": 15,
+                "name": "a"
+            },
+            "sender": {
+                "id": 15,
+                "name": "a"
+            },
+            "reply": {
+                "id": 14,
+                "subject": "xxxx"
+            }
+        },
+        {
+            "id": 2,
+            "subject": "xx",
+            "state": 7,
+            "date": "2015-07-06 16:23:23",
+            "to": "594254655@qq.com",
+            "tag": null,
+            "checker": {
+                "id": 15,
+                "name": "a"
+            },
+            "sender": {
+                "id": 15,
+                "name": "a"
+            },
+            "reply": {
+                "id": 14,
+                "subject": "xxxx"
+            }
+        }
+    ];
 }]);
