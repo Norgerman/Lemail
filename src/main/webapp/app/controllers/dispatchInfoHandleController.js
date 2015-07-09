b3a93b4b26bf96fd3ac92de084b49a2af8ec6ca8
lilelr <lilelr@163.com>
2015-07-09 14:40:18 +0800

    分发者发给处理者通知信息模态框

---
+++ src/main/webapp/app/controllers/dispatchInfoHandleController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yuxiao on 15/7/9.
+ */
+LeMailModule.controller('distributeListController',
+    ['$scope','$http','$sce','$routeParams','$location',
+        function($scope, $http, $sce, $routeParams, $location){
+
+        }]);
