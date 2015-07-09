9a5f69c7ce1d2bea67f5b097ace25a49e4a10aa6
vvliebe@gmail.com <vvliebe@gmail.com>
2015-07-09 22:10:20 +0800

    reset

--- src/main/webapp/app/controllers/replyController.js
+++ src/main/webapp/app/controllers/replyController.js
@@ -25,6 +25,10 @@ LeMailModule.controller('replyController', ['$scope', '$http', '$location', '$ro
         };
 
         $scope.review_mail = function(isPass){
+            if($scope.review_content == ""){
+                alert("审核意见不能为空");
+                return
+            }
             $http({
                 url: '/api/reviewer/checkmail',
                 method: 'GET',
@@ -35,12 +39,36 @@ LeMailModule.controller('replyController', ['$scope', '$http', '$location', '$ro
             }).success(function (response, status, headers, config) {
                 console.log(response);
                 if (response.status == 0) {
-                    alert(response.message);
+                    if(isPass){
+                        alert("邮件已通过审核");
+                        window.location = "/#/reviewer/unchecked";
+                    }
                 } else {
                     alert(response.message);
                 }
             }).error(function (response, status, headers, config) {
                 console.log(response);
             });
+            if(!isPass){
+                $http({
+                    url: '/api/message/send',
+                    method: 'GET',
+                    params: {
+                        to: $scope.reply.sender.id,
+                        content: "审核|"+$scope.review_content,
+                        mail_checked_id: parseInt($scope.mail_id)
+                    }
+                }).success(function (response, status, headers, config) {
+                    console.log(response);
+                    if (response.status == 0) {
+                        alert("成功退回邮件");
+                        window.location = "/#/reviewer/unchecked";
+                    } else {
+                        alert(response.message);
+                    }
+                }).error(function (response, status, headers, config) {
+                    console.log(response);
+                });
+            }
         };
     }]);
